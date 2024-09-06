39931482b77635f9a637575b4bfcfe9b56c3f12d
Rodrigo Saguer <49574554+Extelen@users.noreply.github.com>
2024-09-06 15:25:38 -0400

    Create Enums.cs

---
+++ Programación Avanzada (2024)/Clase 3/Enums.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Enums : MonoBehaviour
+{
+    public enum Nombre
+    {
+        Rodrigo,
+        Gonzalo,
+        Marcelo,
+        Perejil,
+        Yogurlavia,
+        Pikachu
+    }
+
+    public Nombre nombre;
+
+    private void Start()
+    {
+        // ifs
+        if (nombre == Nombre.Rodrigo)
+        {
+            Debug.Log("Soy el profe");
+        }
+        else if (nombre == Nombre.Pikachu)
+        {
+            Debug.Log("Soy amarillo");
+        }
+
+
+        // switch
+        switch (nombre)
+        {
+            case Nombre.Rodrigo:
+
+                Debug.Log("Soy el profe");
+                break;
+        
+            case Nombre.Pikachu:
+            case Nombre.Gonzalo:
+            case Nombre.Marcelo:
+                Debug.Log("Soy pikachu, gonzalo y marcelo");
+                break;
+
+            default:
+                Debug.Log("No soy ninguno jaja xd");
+                
+        }
+
+
+        // switch 2.0
+        string texto = nombre switch
+        {
+            Nombre.Rodrigo => "Me llamo rodrigo",
+            Nombre.Gonzalo => "Soy gonzalo",
+            _ => "No soy ninguno",
+        };
+
+        Debug.Log(texto);
+
+
+
+
+    }
+
+
+}
